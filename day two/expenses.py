f663490c4f75f468a5f86509521627ec0875323b
Bibek Joshi <bebikjoshi118@gmail.com>
2025-03-08 01:13:30 +0545

    day 2 challange

---
+++ day two/expenses.py
@@ -0,0 +1,13 @@
+sunday_expenses = 2344
+monday_expenses  = 3452
+tuesday_expenses = 5003
+wednesday_expenses = 5321
+thursday_expenses  = 9288
+friday_expenses = 2452
+saturday_expenses = 3632
+
+total_expenses = sunday_expenses + monday_expenses+ tuesday_expenses+wednesday_expenses+thursday_expenses+friday_expenses+saturday_expenses
+
+print(f"total expenses {total_expenses}")
+average = total_expenses/7
+print(f"Average expenses is {average}")
