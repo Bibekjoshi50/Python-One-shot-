18643e805ee8da7a8c3adef1b2b23899def4cec8
Bibek Joshi <bebikjoshi118@gmail.com>
2025-03-08 19:34:53 +0545

    comments

---
+++ Day five/comment.py
@@ -0,0 +1,13 @@
+# THis is comment in python programming 
+print("I am a python learnear and begineer ")
+
+
+num1 = 10
+num2 = 5
+
+# addition 
+# adding two numbers 
+result_add = num1 + num2
+
+# printing the result of addition 
+print(f"Addition of {num1} and {num2} is : {result_add}")
