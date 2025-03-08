52251ef84b66506f3de1bfee7d8edfc3a27a56f5
Bibek Joshi <bebikjoshi118@gmail.com>
2025-03-08 19:41:22 +0545

    odd even

---
+++ Day five/oddeven.py
@@ -0,0 +1,13 @@
+x = 35
+
+if x%2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+    # checking another number
+    x = 8 
+    if x%2 == 0:
+        print("Even")
+    else:
+        print("Odd")
