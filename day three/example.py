215e490860f42e83af1dd057613c1d7491a9162c
Bibek Joshi <bebikjoshi118@gmail.com>
2025-03-08 14:16:30 +0545

    exp

---
+++ day three/example.py
@@ -0,0 +1,28 @@
+
+total_books = 563  
+
+averege_rating = 3.45
+
+librarian_name = "Hark Kathayat "
+
+is_open = True
+book_name = ["python programmin ", "Introduction to python programming ", "rich dad poor dad"]
+
+location_coordinates = (28.1, 81.2)
+
+contact_details = {
+    "phone" : "9868736074",
+    "email" : "bebikjoshi118@gmail.com",
+    "address" : "bheemdatt 18 , mahendranagar kanchanpur ,Nepal" 
+}
+
+print("library Information : ")
+print(f"Total number of books : {total_books}")
+print(f"Average rating : {averege_rating}")
+print(f"librarian name : {librarian_name}")
+print(f"Is library open? : {'yes'if is_open else' No'}")
+
+print("list of book names available : {book_names}")
+print("location coordinates : {location_coordinates}")
+print("Contact details : {contact_details}")
+
