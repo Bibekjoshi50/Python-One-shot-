eac3a8180b494afef0ad09a21c53975452f7f723
Bibek Joshi <bebikjoshi118@gmail.com>
2025-03-08 09:11:38 +0545

    datatype

---
+++ day three/datatype.py
@@ -0,0 +1,17 @@
+
+num1 = 10
+num2 = 10.5
+name = "Bibek joshi "
+names = ['bibek joshi', 'manisha joshi','himanshu pant', 'suman pant', 'Bhoj raj joshi' 
+]
+
+
+
+print(type(num1))
+print(type(num2))
+print(type(name))
+print(type(names))
+print(names[1])
+print(names[2])
+
+
