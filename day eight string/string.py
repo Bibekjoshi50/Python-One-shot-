564dcb20a1b0d851dc0e6e43c9401a157704e618
Bibek Joshi <bebikjoshi118@gmail.com>
2025-03-09 13:33:17 +0545

    day eight string

---
+++ day eight string/string.py
@@ -0,0 +1,10 @@
+
+# print("THis is String")
+# print("This is also string")
+
+name = input("Enter your name: ")
+greet = f"Hello {name}, how are you?"
+
+print(greet)
+
+
---
+++ day eight string/stringmethods.py
@@ -0,0 +1,23 @@
+
+name = "bibek Joshi"
+
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(name.title())
+print(name.swapcase())
+print(name.strip())
+print(name.replace("bibek", " bashudev "))
+
+
+names = "hari ,shyam ,rita, sita"
+
+lists = names.split(",")
+
+print(lists)
+
+for n in lists:
+    print(n)
+
+
+
