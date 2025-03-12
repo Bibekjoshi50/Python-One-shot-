feec88f4dd6ac069a3d5074fbff345c7e403a487
Bibek Joshi <bebikjoshi118@gmail.com>
2025-03-12 20:09:50 +0545

    14

---
+++ day 14/book.py
@@ -0,0 +1,10 @@
+
+
+books = [" Book A", "Book B","Book C", "Book D"]
+for book in books:
+    if book == "Book C":
+        print("Found the book! Stopping search.")
+        break
+    print(f"Checking {book}")
+
+    print("Search ended.")
